09e853623ba797a455ab9ce62cb17be6a21d4916
Poojitha.B <poojitha.b@science.christuniversity.in>
2020-05-11 22:13:31 +0530

    Update cachematrix.R
    my assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# this funtion is used to get median
 makeCacheMatrix <- function(x = matrix()) {
+    m<-NULL
+    c <- function(y){
+        
+        x<<-y
+        m<<-NULL
+        
+    }
+    p <- function() x
+    cmediana <- function(median) m <<- solve
+    dmediana <- function() m
+    list(c = c, c = cr,
+         cmediana = cmediana,
+         dmediana = dmediana)
 
 }
 
 
 ## Write a short comment describing this function
+#This function invert matrix e get median
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$configurarmediana()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    matrix<-x$configurar
+    m<-median(matrix, ...)
+    x$configurarmediana(m)
+    m
 }
